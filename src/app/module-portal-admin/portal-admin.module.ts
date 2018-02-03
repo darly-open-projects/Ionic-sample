14b7bd3ed7c8a1a23a7cc3eb9b2ec14099537472
Daria Lymanska <darly.solutions.first@gmail.com>
2018-02-03 12:33:19 +0200

    Add modules to Portal admin

--- src/app/module-portal-admin/portal-admin.module.ts
+++ src/app/module-portal-admin/portal-admin.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import {DistributionModule} from './module-distribution/distribution.module';
+import {CampaignsModule} from './module-campaigns/campaigns.module';
 
 
 @NgModule({
-  imports: [IonicPageModule]
+  imports: [
+    IonicPageModule,
+    DistributionModule,
+    CampaignsModule
+  ]
 })
 export class PortalAdminModule {
 }
